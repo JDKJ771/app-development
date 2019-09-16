fd714376bb9943bb307a6584ed2eefb2fe5b5f9d
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2019-09-16 10:48:46 -0600

    Add files via upload

--- k.py
+++ k.py
@@ -1,7 +1,16 @@
 import tkinter as tk
+def hi():
+    name=entry.get()
+    print('hi', name)
 root = tk.Tk()
 
-textLabel = tk.Label(root, text = "hi")
+textLabel = tk.Label(root, text = "hi put your name here: ")
 textLabel.pack()
+root.title("saying hi")
+entry=tk.Entry(root)
+entry.pack()
+button=tk.Button(root, text = 'done', command=hi)
+button.pack()
+
 
 root.mainloop()
