adba4a9d05a0d9096443f53ead4e8df505dcafd8
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2019-09-23 14:59:00 -0600

    Add files via upload

---
+++ spam!!!!.py
@@ -0,0 +1,16 @@
+num = input("number: ")
+num = int(num)
+
+
+if num > 100:
+    print("number too big")
+
+while True:
+    password = input("whats your password: ")
+
+    if password =="JedJohnson":
+        print ("hi jed welcome back")
+
+else:
+    print("access Denied")
+    
