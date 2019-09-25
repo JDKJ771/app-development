d0b3d67f9797aa049d5a36711189958faaab12ed
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2019-09-25 13:52:49 -0600

    Update select time.py
    7/25/19
                so today I wrote this piece of software and plan to keep updating it as well. this will go on a different piece of software that I am also building

--- select time.py
+++ select time.py
@@ -2,8 +2,63 @@ import tkinter as tk
 
 root = tk.Tk()
 
+def A8():
+    textLabel = tk.Label(root, text = "what time does A3 start and end : ")
+    textLabel.pack()
+    root.title("Class sceadule")
+    entry=tk.Entry(root)
+    entry.pack()
+    entry=tk.Entry(root)
+    entry.pack()
+    button=tk.Button(root, text = 'A8', command=A8)
+    button.pack()
+
+def A7():
+    textLabel = tk.Label(root, text = "what time does A3 start and end : ")
+    textLabel.pack()
+    root.title("Class sceadule")
+    entry=tk.Entry(root)
+    entry.pack()
+    entry=tk.Entry(root)
+    entry.pack()
+    button=tk.Button(root, text = 'A8', command=A8)
+    button.pack()
+
+def A6():
+    textLabel = tk.Label(root, text = "what time does A3 start and end: ")
+    textLabel.pack()
+    root.title("Class sceadule")
+    entry=tk.Entry(root)
+    entry.pack()
+    entry=tk.Entry(root)
+    entry.pack()
+    button=tk.Button(root, text = 'A7', command=A7)
+    button.pack()
+    
+def A5():
+   textLabel = tk.Label(root, text = "what time does A5 start and end: ")
+    textLabel.pack()
+    root.title("Class sceadule")
+    entry=tk.Entry(root)
+    entry.pack()
+    entry=tk.Entry(root)
+    entry.pack()
+    button=tk.Button(root, text = 'A6', command=A6)
+    button.pack()
+
+def A4():
+    textLabel = tk.Label(root, text = "what time does A4 start and end : ")
+    textLabel.pack()
+    root.title("Class sceadule")
+    entry=tk.Entry(root)
+    entry.pack()
+    entry=tk.Entry(root)
+    entry.pack()
+    button=tk.Button(root, text = 'A5', command=A5)
+    button.pack()
+
 def A3():
-    textLabel = tk.Label(root, text = "what time do these classes start and end: ")
+    textLabel = tk.Label(root, text = "what time does A3 start and end : ")
     textLabel.pack()
     root.title("Class sceadule")
     entry=tk.Entry(root)
@@ -13,7 +68,7 @@ def A3():
     button=tk.Button(root, text = 'A4', command=A2)
     button.pack()
 def A2():
-    textLabel = tk.Label(root, text = "what time do these classes start and end: ")
+    textLabel = tk.Label(root, text = "what time does A2 start and end: ")
     textLabel.pack()
     root.title("Class sceadule")
     entry=tk.Entry(root)
@@ -23,9 +78,10 @@ def A2():
     button=tk.Button(root, text = 'A3', command=A3)
     button.pack()
 
-    
+button=tk.Button(root, text = 'A1')
+button.pack()
 
-textLabel = tk.Label(root, text = "what time do these classes start and end: ")
+textLabel = tk.Label(root, text = "what time does A1 start and end: ")
 textLabel.pack()
 root.title("Class sceadule")
 entry=tk.Entry(root)
