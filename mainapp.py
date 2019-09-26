3e9a50675bf7842afca9ab2e440ac97eddc103ca
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2019-09-26 11:13:00 -0600

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -1,14 +1,15 @@
 import tkinter as tk
 
+def the():
+    import k
+
 root = tk.Tk()
 
 textLabel = tk.Label(root, text = "you are now all set up. use your phone as you would normaly would.") 
 textLabel = tk.Label(root, text = "we will alert you when you are all set up") 
 textLabel.pack()
-root.title("scheduler for classes")
-entry=tk.Entry(root)
-entry.pack()
-
-
+root.title("class sceaduler")
+list=tk.List():
+      the
 
 root.mainloop()
