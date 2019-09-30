ab16d1da40b1265960cb78c0c68dd75f2aecfad7
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2019-09-30 13:47:46 -0600

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -9,7 +9,16 @@ textLabel = tk.Label(root, text = "you are now all set up. use your phone as you
 textLabel = tk.Label(root, text = "we will alert you when you are all set up") 
 textLabel.pack()
 root.title("class sceaduler")
-setlist = tk.List():
-      the
 
+setlist = tk.List(the):
+ 
+def alert():
+    import winsound
+    frequency = 2500  # Set Frequency To 2500 Hertz
+    duration = 1000  # Set Duration To 1000 ms == 1 second
+    winsound.Beep(frequency, duration)
+    setlist = tk.List(the)
+    
+        
+        
 root.mainloop()
