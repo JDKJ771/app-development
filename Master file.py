400445e5c7c483474769732aa76d2273da7b16a1
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2019-11-07 18:35:54 -0700

    Update Master file.py

--- Master file.py
+++ Master file.py
@@ -116,108 +116,108 @@ def time2():
     #end
     root.mainloop()
 #end Def
-def k():
-    #C day
-    def cday():
-        textLabel = tk.Label(root, text = "what are your B day classes: ")
-        textLabel.pack()
-        root.title("Class sceadule")
 
-        entryC1=tk.Entry(root)
-        entryC1.pack()
+#C day
+def cday():
+     textLabel = tk.Label(root, text = "what are your B day classes: ")
+     textLabel.pack()
+     root.title("Class sceadule")
 
-        entryC2=tk.Entry(root)
-        entryC2.pack()
+     entryC1=tk.Entry(root)
+     entryC1.pack()
 
-        entryC3=tk.Entry(root)
-        entryC3.pack()
-        button=tk.Button(root, text = 'next', command=time2)
-        button.pack()
+     entryC2=tk.Entry(root)
+     entryC2.pack()
+
+     entryC3=tk.Entry(root)
+     entryC3.pack()
+     button=tk.Button(root, text = 'next', command=time2)
+     button.pack()
 
 
     #B day
 
-    def bday():
-        textLabel = tk.Label(root, text = "what are your B day classes: ")
-        textLabel.pack()
-        root.title("Class sceadule")
+ def bday():
+     textLabel = tk.Label(root, text = "what are your B day classes: ")
+     textLabel.pack()
+     root.title("Class sceadule")
 
-        entryB1=tk.Entry(root)
-        entryB1.pack()
+     entryB1=tk.Entry(root)
+     entryB1.pack()
 
-        entryB2=tk.Entry(root)
-        entryB2.pack()
+     entryB2=tk.Entry(root)
+     entryB2.pack()
 
-        entryB3=tk.Entry(root)
-        entryB3.pack()
+     entryB3=tk.Entry(root)
+     entryB3.pack()
 
-        entryB4=tk.Entry(root)
-        entryB4.pack()
+     entryB4=tk.Entry(root)
+     entryB4.pack()
 
-        entryB5=tk.Entry(root)
-        entryB5.pack()
+     entryB5=tk.Entry(root)
+     entryB5.pack()
 
-        entryB6=tk.Entry(root)
-        entryB6.pack()
+     entryB6=tk.Entry(root)
+     entryB6.pack()
 
-        entryB7=tk.Entry(root)
-        entryB7.pack()
+     entryB7=tk.Entry(root)
+     entryB7.pack()
 
-        entryB8=tk.Entry(root)
-        entryB8.pack()
+     entryB8=tk.Entry(root)
+     entryB8.pack()
 
-        entryB9=tk.Entry(root)
-        entryB9.pack()
-        button=tk.Button(root, text = 'next', command=cday)
-        button.pack()
+     entryB9=tk.Entry(root)
+     entryB9.pack()
+     button=tk.Button(root, text = 'next', command=
+     button.pack()
 
 
       #A day  
 
-    def __init__(self):
-        self.root = tk.Tk()
-        self.label = tk.Label(text="")
-        self.label.pack()
-        self.update_clock()
-        self.root.mainloop()
+ def __init__(self):
+     self.root = tk.Tk()
+     self.label = tk.Label(text="")
+     self.label.pack()
+     self.update_clock()
+     self.root.mainloop()
 
-    def update_clock(self):
-        now = time.strftime("%H:%M:%S")
-        self.label.configure(text=now)
-        self.root.after(1000, self.update_clock)
-    #end def
-    textLabel = tk.Label(root, text = "What are your A day classes: ")
-    textLabel.pack()
-    root.title("Class sceadule")
+  def update_clock(self):
+      now = time.strftime("%H:%M:%S")
+      self.label.configure(text=now)
+      self.root.after(1000, self.update_clock)
+  #end def
+  textLabel = tk.Label(root, text = "What are your A day classes: ")
+  textLabel.pack()
+  root.title("Class sceadule")
 
-    entryA1=tk.Entry(root)
-    entryA1.pack()
+  entryA1=tk.Entry(root)
+  entryA1.pack()
 
-    entryA2=tk.Entry(root)
-    entryA2.pack()
+  entryA2=tk.Entry(root)
+  entryA2.pack()
 
-    entryA3=tk.Entry(root)
-    entryA3.pack()
+  entryA3=tk.Entry(root)
+  entryA3.pack()
 
-    entryA4=tk.Entry(root)
-    entryA4.pack()
+  entryA4=tk.Entry(root)
+  entryA4.pack()
 
-    entryA5=tk.Entry(root)
-    entryA5.pack()
+  entryA5=tk.Entry(root)
+  entryA5.pack()
 
-    entryA6=tk.Entry(root)
-    entryA6.pack()
+  entryA6=tk.Entry(root)
+  entryA6.pack()
 
-    entryA7=tk.Entry(root)
-    entryA7.pack()
+  entryA7=tk.Entry(root)
+  entryA7.pack()
 
-    entryA8=tk.Entry(root)
-    entryA8.pack()
+  entryA8=tk.Entry(root)
+  entryA8.pack()
 
-    entryA9=tk.Entry(root)
-    entryA9.pack()
-    button=tk.Button(root, text = 'next', command=bday)
-    button.pack()
+  entryA9=tk.Entry(root)
+  entryA9.pack()
+  button=tk.Button(root, text = 'next', command=bday)
+  button.pack()
 
 A1 = entryA1.get
 
