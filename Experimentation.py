78f3bfe6181167f21901077675116a7b43768927
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2019-12-09 13:39:37 -0700

    Update Experimentation.py

--- Experimentation.py
+++ Experimentation.py
@@ -1,2 +1,12 @@
-from playsound import playsound
-playsound('audio.mp3')
+from tkinter import * 
+
+def show():
+    p = password.get() #get password from entry
+    print(p)
+
+
+app = Tk()   
+password = StringVar() #Password variable
+passEntry = Entry(app, textvariable=password, show='*').pack() 
+submit = Button(app, text='Show Console',command=show).pack()      
+app.mainloop() 
