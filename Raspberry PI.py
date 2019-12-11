af1d309e09fc612bbc0c546891e616cb6aef9f45
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2019-12-11 09:27:01 -0700

    Update Raspberry PI.py

--- Raspberry PI.py
+++ Raspberry PI.py
@@ -1,10 +1,26 @@
-import tkinter as tk
+Select_name = input('what would you like to call me?')
+whatIsYourName = input('what would like me to call you')
+class AI:
+      
+      def __init__(self, name):
+            self.name = name
+            
+P1 = AI(Select_name)
 
-root = tk.Tk()
+def list():
+      print('define words \n tell you my name (make sure you type WHat is your name) \n What i am (type what are you) \n 
+
+whatIsYourName = ('my name' + P1)
+WhatCanIDO = 'here is a list of things i can do'
+WhatAreYou = 'I am a ANI or Artificial Narrow Intelligence'
+            
+Maininput = input('Hello what can i do for you')
 
 
+if Maininput == 'what is your name':
+      print(whatIsYourName)
+            
+if Maininput == 'what are you:
+            print(WHatCanIDo)
 
-def Home():
-      root.title('Home')
-
-      button = tk.Button(root, text = 'Settings' command=
+    
