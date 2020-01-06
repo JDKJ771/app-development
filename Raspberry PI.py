8c454e5da0fe1e6d28d293d6abe4875f7869f484
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2020-01-06 09:17:19 -0700

    Update Raspberry PI.py

--- Raspberry PI.py
+++ Raspberry PI.py
@@ -1,21 +1,11 @@
 import datetime
-Select_name = input('what would you like to call me?')
-whatIsYourName = input('what would like me to call you')
-class AI:
-      
-      def __init__(self, name):
-            self.name = name
-            
-P1 = AI(Select_name)
-
 def list():
       print('define words(type define than the word you want to define) \n tell you my name (make sure you type What is your name) \n What i am (type what are you) \n Tell you the time \n'
 
-whatIsYourName = ('my name' + P1)
 WhatCanIDO = 'here is a list of things i can do'
 WhatAreYou = 'I am a ANI or Artificial Narrow Intelligence'
-now = datetime.datetime.now()
-currenttime = now.hour + ':', now.min
+cdt = datetime.datetime.now()
+currenttime = (cdt.hour) + ':' + (cdt.min)
 
             
             
@@ -23,13 +13,13 @@ Maininput = input('Hello what can i do for you')
 
 
 if Maininput == 'what is your name':
-      print(whatIsYourName)
+      print('my name is Pixel')
             
 if Maininput == 'what are you:
             print(WHatCanIDo)
             
 if Maininput == 'Hey' + Select_name:
-            print('hello' + whatIsYourName+ ',What can i help you with)
+            print('hello Jed, What can i help you with)
 
 if Maininput == 'what time is it':
                   print(currenttime)
