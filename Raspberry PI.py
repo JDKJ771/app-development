9886262cba7c6bc80aa377194cfc6f7424c3fd7a
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2020-08-25 10:31:39 -0600

    Update Raspberry PI.py

--- Raspberry PI.py
+++ Raspberry PI.py
@@ -1,30 +1,22 @@
+from picamera import PiCamera
 import datetime
-def list():
-      print('define words(type define than the word you want to define) \n tell you my name (make sure you type What is your name) \n What i am (type what are you) \n Tell you the time \n'
+import time
+import SpeachRecognition
+import pygame
+import gtts
+import google
+import audyo
+from gpiozero import button
+###Camera commands
+camera = Picamera()
 
-WhatCanIDO = 'here is a list of things i can do'
-WhatAreYou = 'I am a ANI or Artificial Narrow Intelligence'
-cdt = datetime.datetime.now()
-currenttime = (cdt.hour) + ':' + (cdt.min)
+camera.start_preview()
+camera.dislplay
 
-            
-            
-Maininput = input('Hello what can i do for you')
+###GPIO Pins
+button = Button(2)
 
+if button.is_pressed:
+      camera.capture("home/pi/desktop")
 
-if Maininput == 'what is your name':
-      print('my name is Pixel')
-            
-if Maininput == 'what are you:
-            print(WHatCanIDo)
-            
-if Maininput == 'Hey' + Select_name:
-            print('hello Jed, What can i help you with)
-
-if Maininput == 'what time is it':
-                  print(currenttime)
-                  
-            
-            
-
-    
+###Virtual assistant Start
